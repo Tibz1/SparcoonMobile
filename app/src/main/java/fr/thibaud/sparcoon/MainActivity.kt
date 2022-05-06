062665712d50d14e6be4bf57ddf812863b7be401
Tibz1 <thibaud62151@gmail.com>
2022-05-06 17:10:04 +0200

    main layout + home  layout

--- app/src/main/java/fr/thibaud/sparcoon/MainActivity.kt
+++ app/src/main/java/fr/thibaud/sparcoon/MainActivity.kt
@@ -2,10 +2,17 @@ package fr.thibaud.sparcoon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.thibaud.sparcoon.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Injecter le fragment dans a boite (fragment_container)
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, HomeFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
